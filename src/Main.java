1e4cd19b25921904afda9a06eaad311c74e580c5
ESE58 <shepshop@ya.ru>
2021-10-04 02:17:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 1;
+        int milePrice = 20;
+        int milesPerTicket = ticketPrice / milePrice;
+
+        System.out.println(milesPerTicket);
+        }
+}
